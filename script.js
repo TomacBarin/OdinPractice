5c03deeb4224626287edf2ef4305c22f80970643
Tomac Barin Jansson <tomacbarin@me.com>
2025-10-17 08:18:39 +0200

    Andra påbörjad.

--- script.js
+++ script.js
@@ -5,3 +5,8 @@ redP.textContent = "Hey I'm red!";
 container.appendChild(redP);
 redP.style.color = ("red");
 
+const blueH3 = document.querySelector("h3");
+blueH3.classList.add("blueH3");
+blueH3.textContent = "I'm a blue h3!";
+container.appendChild(blueH3);
+blueH3.style.color = ("blue");
