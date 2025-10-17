8ab2926f8873da9f4d3da1ead0c7a0fa32af1c34
Tomac Barin Jansson <tomacbarin@me.com>
2025-10-17 21:31:38 +0200

    Script added from knowledge

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
 const container = document.querySelector("#container");
+
 const redP = document.createElement("p");
 redP.classList.add("redP");
 redP.textContent = "Hey I'm red!";
 container.appendChild(redP);
 redP.style.color = ("red");
 
-const blueH3 = document.querySelector("h3");
-blueH3.classList.add("blueH3");
-blueH3.textContent = "I'm a blue h3!";
-container.appendChild(blueH3);
-blueH3.style.color = ("blue");
+const answerP = document.createElement("p");
+answerP.classList.add("answerP");
+answerP.textContent = "Hey red, I'm dad!";
+container.appendChild(answerP);
+answerP.style.fontStyle = ("italic");
+
+
+
