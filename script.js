747d0474a8f92948ad118d841af9cf257aba80e4
Tomac Barin Jansson <tomacbarin@me.com>
2025-10-17 22:02:07 +0200

    Final push.

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const container = document.querySelector("#container");
 
 const redP = document.createElement("p");
-redP.classList.add("redP");
+// redP.classList.add("redP");
 redP.textContent = "Hey I'm red!";
 container.appendChild(redP);
 redP.style.color = ("red");
